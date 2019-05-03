5390920b7a1501df782bd4307038f36b98cfccd3
grexess <grexess@googlemail.com>
2019-05-03 18:26:47 +0200

    beforeDelete

--- script/data.js
+++ script/data.js
@@ -114,5 +114,60 @@ xdata = [{
     id: '?',
     txt1: 'Ipsum',
     txt2: '?'
+  },
+  {
+    imgY: '014Y.png',
+    imgO: '014O.png',
+    firstname: 'Finn',
+    lastname: 'Spannaus',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
+  },
+  {
+    imgY: '015Y.png',
+    imgO: '015O.png',
+    firstname: 'Gustav',
+    lastname: 'Rost',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
+  },
+  {
+    imgY: '016Y.png',
+    imgO: '016O.png',
+    firstname: 'Anton',
+    lastname: 'Stoppa',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
+  },
+  {
+    imgY: '017Y.png',
+    imgO: '017O.png',
+    firstname: 'Janko',
+    lastname: 'Grünzig',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
+  },
+  {
+    imgY: '018Y.png',
+    imgO: '018O.png',
+    firstname: 'Lisa',
+    lastname: 'Mindergasov',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
+  },
+  ,
+  {
+    imgY: '019Y.png',
+    imgO: '019O.png',
+    firstname: 'Maximilian',
+    lastname: 'Cremer',
+    id: '?',
+    txt1: 'Ipsum',
+    txt2: '?'
   }
 ]
